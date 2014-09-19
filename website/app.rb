8722e96a76876966f1857398043ec5cef73f9861
Tyler Lemburg <tyler.lemburg@archrival.com>
2014-09-19 16:23:51 -0500

    Got 3 RSS feeds running

--- website/app.rb
+++ website/app.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 require 'pp'
-require 'xmlsimple'
-require 'activerecord'
+require 'active_record'
+require 'rss'
+require 'open-uri'
 
-
-class NewsItem < ActiveRecord::Model
+class NewsItem < ActiveRecord::Base
   
 end
 
@@ -13,22 +13,35 @@ get '/' do
 end
 
 get '/scan' do
+  out = []
+
   # get the list of RSS feeds
   feeds = [
-    'http://www.omaha.com/search/?q=&t=article&l=25&d=&d1=&d2=&s=start_time&sd=desc&c[]=huskers/football*&f=rss'
+    'http://www.omaha.com/search/?q=&t=article&l=25&d=&d1=&d2=&s=start_time&sd=desc&c[]=huskers/football*&f=rss',
+    'http://www.huskers.com/rss.dbml?db_oem_id=100&RSS_SPORT_ID=22&media=news',
+    'http://espn.go.com/blog/feed?blog=collegesnebraska'
   ]
 
-  out = ''
+  feeds.each do |url|
+    open(url) do |rss|
+        feed = RSS::Parser.parse(rss, false)
 
-  feeds.each do |feed|
-    xml_data = Net::HTTP.get_response(URI.parse(feed)).body
-    data = XmlSimple.xml_in(xml_data)
-
-    
-
-    out << pp(data.inspect)
+        feed.items.each do |item|
+            guid = item.guid.content
+            if NewsItem.where(:guid => guid).first.nil?
+                new_item = NewsItem.create(
+                    :title => item.title,
+                    :guid => guid,
+                    :link => item.link,
+                    :original_publish_date => item.pubDate
+                )
+                out << 'Inserted article with title: ' + item.title
+            else 
+                out << 'Skipped article with title: ' + item.title
+            end
+        end
+    end
   end
 
-  out
-  
+  out.join('<br>')
 end
