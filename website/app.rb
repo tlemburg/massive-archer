1c7e0863e383e41de279c552a26e037aa11bd748
tlemburg <trlemburg@gmail.com>
2014-09-22 14:11:28 -0500

    Some small QOL improvements

--- website/app.rb
+++ website/app.rb
@@ -11,7 +11,7 @@ get '/' do
 
     news_items = news_items.map do |item|
         markdown.render(item.site_markdown.sub('item_link', item.link))
-    end.join('<br>')
+    end.join
 
 	erb :index, :locals => {:recent_items_html => news_items}
 end
