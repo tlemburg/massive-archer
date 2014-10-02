b5325b882b60831e5f51944d60958cb86ba66fcd
tlemburg <trlemburg@gmail.com>
2014-10-02 14:26:00 -0500

    Integrated payment form to create new account

--- config.ru
+++ config.ru
@@ -4,12 +4,18 @@ require 'sinatra'
 require 'rss'
 require 'redcarpet'
 require 'curb'
+require 'stripe'
 
 Dir['./models/*'].each {|f| require f}
 Dir['./utils/*'].each {|f| require f}
 require './app'
 
 ENV['RACK_ENV'] = ENV['RACK_ENV'] || 'development'
+ENV['STRIPE_TEST_KEY'] = ENV['STRIPE_TEST_KEY'] || 'pk_test_C1nkt9dG6QEJfyHjiwnZ1MWc'
+# read the stripe secret from the config file, if it doesn't exist.
+if !ENV['STRIPE_TEST_SECRET'] 
+    ENV['STRIPE_TEST_SECRET'] = JSON.parse(File.read('./.config/config.json'))['stripe_test_secret']
+end
 
 if ENV['RACK_ENV'] == 'production'
   ActiveRecord::Base.establish_connection(ENV['CLEARDB_DATABASE_URL'])
--- routes/auth.rb
+++ routes/auth.rb
@@ -21,3 +21,38 @@ get '/logout' do
   session.clear
   redirect '/'
 end
+
+get '/create-account' do 
+  erb :create_account
+end
+
+post '/create-account' do
+  # check for already existing username:
+  unless User.find_by(:username => params[:username]).nil?
+    puts 'user with username already exists'
+    redirect '/create-account'
+  end
+
+  Stripe.api_key = ENV['STRIPE_TEST_SECRET']
+
+  # need to send this token up to stripe to create a recurring subscription
+  customer = Stripe::Customer.create(
+    {
+      :card => params[:stripeToken],
+      :plan => 'standard',
+      :email => params[:stripeEmail]
+    }
+  )
+
+  u = User.new({
+    :username => params["username"],
+    :is_admin => false,
+    :is_reviewer => false,
+    :subscription_valid => true,
+    :stripe_customer_id => customer.id
+  })
+
+  u.password = params[:password]
+  u.save
+
+end
